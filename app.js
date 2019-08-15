f2c065019910760ed3319b5485cb74df58cea038
Imran Rahman <imran7147@gmail.com>
2019-08-15 16:03:50 +0100

    added conditional styling for turns

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
 			this.gameIsRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
+			this.turns = [];
 		},
 
 		attack: function() {
@@ -29,7 +30,12 @@ new Vue({
 		},
 
 		specialAttack: function() {
-			this.monsterHealth -= this.calculateDamage(10, 20);
+			var damage = this.calculateDamage(10, 20);
+			this.monsterHealth -= damage;
+			this.turns.unshift({
+				isPlayer: true,
+				text: "Player hits Monster for " + damage
+			});
 			//check if player won and end the function
 			if (this.checkWin()) {
 				return;
@@ -44,6 +50,11 @@ new Vue({
 			} else {
 				this.playerHealth = 100; //otherwise just set back to 100
 			}
+
+			this.turns.unshift({
+				isPlayer: true,
+				text: "Player heals for 10"
+			});
 
 			this.monsterAttacks();
 		},
